6c302b38cf4ac782163f38922d0d58936100713f
JaeYeon <w2109@e-mirim.hs.kr>
2022-06-29 11:38:45 +0900

    html,css,js addNow()

--- 20220608_school cafeteria/js/main.js
+++ 20220608_school cafeteria/js/main.js
@@ -32,3 +32,33 @@ toggleMenu("nav-toggle", "nav-list");
 //     console.log("Hello World");
 // }
 // say();
+
+const addNow = (mainCardId) => {
+    //html -> js
+    const mainCard = document.getElementById(mainCardId);
+    //지금 몇시? 다음 식사 -> 몇번째 카드인지
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    // console.log(`${hour}시 ${minute}분`);
+    minute = hour * 60 + minute; //현재 시와 분을 통해 분으로 단위 통일
+
+    //지금 시각 -> index
+    //조식 끝 :7:30 ->1
+    //중식 끝:13:10->2
+    //석식 끝:18:10->0
+    //다음 식사 -> 몇번째 카드인지
+    index = 0;
+    if (minute >= 18 * 60 + 10) { //10:10 <= now
+        index = 0;
+    } else if (minute >= 13 * 60 + 10) { //13:10<=now
+        index = 2;
+    } else if (minute >= 7 * 60 + 30) { //7:30<=now
+        index = 1;
+    }
+    let selectedCard = mainCard.getElementsByClassName('card')[index];
+    //.now 클래스 추가
+    selectedCard.classList.add('now');
+}
+
+addNow('main-card');
